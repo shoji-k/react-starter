801e07f767655ef53a4cd0ede5603b29d35b65eb
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-04-07 18:44:56 +0900

    css/style/file loader installed

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,28 @@ module.exports = {
   output: {
     path: __dirname + '/public',
     filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 }
