6bc00c5c2ae3ad1af1318074e0a225d89b94acc9
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-04-26 16:50:34 +0900

    generate index.html by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const mode = isProduction ? 'production' : 'development'
@@ -8,48 +10,45 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/public',
-    filename: 'main.js'
+    filename: '[name].[hash].js',
   },
-  devtool: isProduction ? 'source-map' :'inline-source-map',
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
   devServer: {
     contentBase: './public',
     historyApiFallback: true,
-    hot: true
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/assets/index.html',
+    }),
   ],
 }
