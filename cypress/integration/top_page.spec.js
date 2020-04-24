910553012aa70139e7aa78dda18b9dad99092f53
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-04-24 21:36:33 +0900

    add tests to win

--- cypress/integration/top_page.spec.js
+++ cypress/integration/top_page.spec.js
@@ -1,7 +1,25 @@
-describe('Access Top page', () => {
-  it('can get title', () => {
-    cy.visit('/')
+const one = 'X'
+const two = 'O'
 
-    cy.contains('Next player: X')
+describe('Access Top page', () => {
+  it('can get the title', () => {
+    cy.visit('/')
+    cy.contains('Next player: ' + one)
+  })
+
+  it('can switch a player', () => {
+    cy.get('[data-cy=cell5]').click().contains(one)
+    cy.contains('Next player: ' + two)
+  })
+
+  it('can win by player X', () => {
+    cy.get('[data-cy=cell1]').click().contains(two)
+    cy.contains('Next player: ' + one)
+    cy.get('[data-cy=cell3]').click().contains(one)
+    cy.contains('Next player: ' + two)
+    cy.get('[data-cy=cell9]').click().contains(two)
+    cy.contains('Next player: ' + one)
+    cy.get('[data-cy=cell7]').click().contains(one)
+    cy.contains('Winner: ' + one)
   })
 })
--- src/Board.js
+++ src/Board.js
@@ -3,7 +3,9 @@ import Square from './Square'
 
 function Board(props) {
   const renderSquare = (i) => {
-    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
+    return (
+      <Square i={i} value={props.squares[i]} onClick={() => props.onClick(i)} />
+    )
   }
 
   return (
